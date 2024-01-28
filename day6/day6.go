ef45703b31f701ee7928665da795d301a7fcddb0
Irene Jaramillo <ijaramil@starbucks.com>
2024-01-27 16:42:15 -0800

    Day 6: Part 1 complete

--- day6/day6.go
+++ day6/day6.go
@@ -4,39 +4,52 @@ import (
 	"fmt"
 )
 
-var mt=[...]int{7,15,30}
-var md=[...]int{9,40,200}
-var maxT int
+//TEST
+//var mt=[...]int{7,15,30}
+//var md=[...]int{9,40,200}
 
-func iterRace()int{
-	var mid int
-	for i:=0; i < 3; i++{
-		maxT=mt[i]
-		mid=maxT+1/2
-	}
-	return mid
+//REAL
+//Time:        35     93     73     66
+//Distance:   212   2060   1201   1044
+var mt=[...]int{35,93,73,66}
+var md=[...]int{212,2060,1201,1044}
+
+var total=0
+
+func mltp(num int){
+
 }
 
-func calcDistance(m int) int{
+func iterRace() int{
+	var tc [4]int
+	for i:=0; i < 4; i++{
+		tc[i]=calcDistance(i)	
+	}
+		fmt.Print(tc[0],tc[1],tc[2],tc[3])
+	
+	return tc[0]*tc[1]*tc[2]*tc[3]
+}
+
+func calcDistance(i int) int{
+	var maxT,maxD=mt[i],md[i]
 	var distance int
-	count:=1
-	m1,m2:=maxT-1,1
-	for ;m > 0;m-- {
+	count:=0
+	m1,m2:=maxT,0
+	for ;m1 >= 0;m1-- {
 		distance=m1*m2
 		m2++
-		m1--
 		fmt.Println(distance)
 
-		if distance < maxT{
-			continue
-		}else{
+		if distance > maxD{
 			count++
+		}else{
+			continue
 		}
 	}
 	return count
 }
 
 func main(){
-	maxT=mt[2]
-	fmt.Println(calcDistance((maxT+1)/2))
+	rt:=iterRace()
+	fmt.Println("Count:", rt)
 }
